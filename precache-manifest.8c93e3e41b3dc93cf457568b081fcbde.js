8f0c72d7620ba7825f6dec0be1259f99eb49431a
Nagma Bardoi <nagmabardoi@gmail.com>
2020-02-22 22:08:42 -0600

    Updates

---
+++ precache-manifest.8c93e3e41b3dc93cf457568b081fcbde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8854d4223a1234426fd66595742db499",
+    "url": "/marvel-clicky-game/index.html"
+  },
+  {
+    "revision": "2dede5089c102e3e69b4",
+    "url": "/marvel-clicky-game/static/css/main.0fa3d89d.chunk.css"
+  },
+  {
+    "revision": "67f70758f1eebddc78c0",
+    "url": "/marvel-clicky-game/static/js/2.b261041d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/marvel-clicky-game/static/js/2.b261041d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dede5089c102e3e69b4",
+    "url": "/marvel-clicky-game/static/js/main.2016e9ca.chunk.js"
+  },
+  {
+    "revision": "c947bb9f1721e71cbfea",
+    "url": "/marvel-clicky-game/static/js/runtime-main.fa7df4d5.js"
+  }
+]);
